0a45875c19abf9803b09b18a2f19d34c1edfff99
Omar <omar_zikry@github.com>
2018-01-23 15:30:11 +0200

    services nav bar changes

---
+++ services/js/scripts.js
@@ -0,0 +1,30 @@
+////////////////nav bar
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementsById("main").style.marginLeft = "0px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
+
+
+
+var windowWidth = $(window).width();
+if(windowWidth > 768){
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+    
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft= "0";
+        document.body.style.backgroundColor = "white";
+    }
+}
