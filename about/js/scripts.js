817ca2a9dc137774b94dbc27f7dd1b0f39d08f5b
Omar <omar_zikry@github.com>
2018-01-23 20:38:24 +0200

    added nav to about

---
+++ about/js/scripts.js
@@ -0,0 +1,28 @@
+////////////////nav bar
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementsById("main").style.marginLeft = "0px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+
+
+var windowWidth = $(window).width();
+if(windowWidth > 768){
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+    
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft= "0";
+    }
+}
