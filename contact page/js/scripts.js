9880f3e1f39a87a286844194384a0bf72931effa
Omar <omar_zikry@github.com>
2018-01-23 20:51:18 +0200

    I added a nav to contact page

--- contact page/js/scripts.js
+++ contact page/js/scripts.js
@@ -1,21 +1,30 @@
-$('#contact-form').submit(function(e){
-var name = document.getElementById('inputName'),
-    email = document.getElementById('inputEmail'),
-    message = document.getElementById('inputMessage');
 
-    if(!name.value || !email.value || !message.value){
-alertify.error('Please check your enteries');
 
-    } else{
-        $.ajax({
-            url: "https://formspree.io/omarzikry69@gmail.com", 
-            method: "POST",
-            data: $(this).serialize(),
-            dataType: "json"
-        }); 
-        e.preventDefault()
-        $(this).get(0).reset()
-        alertify.success('Message sent')
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementsById("main").style.marginLeft = "0px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
+
+
+
+var windowWidth = $(window).width();
+if(windowWidth > 768){
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
     }
-})
-
+    
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft= "0";
+        document.body.style.backgroundColor = "white";
+    }
+}
