00542a276a38ca3b6e86dfbdf00a2c29fa8f4ba1
Omar <omar_zikry@github.com>
2018-02-04 23:54:38 +0200

    vemo website

---
+++ vemo/js/scripts.js
@@ -0,0 +1,41 @@
+//==================
+// NAVBAR
+//==================
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+
+
+var windowWidth = $(window).width();
+if(windowWidth > 768){
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "23%";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+    
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.body.style.backgroundColor = "white";
+    }
+}
+
+
+//removable nav
+    $(window).scroll(function(){
+    if ($(document).scrollTop() > 350){
+        $('header').hide(500)
+    }});
+
+    
+    $(window).scroll(function(){
+        if ($(document).scrollTop() < 350){
+            $('header').show(500)
+        }});
