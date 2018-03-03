d6de1f3771761fdf02fa6fd5ddf536c55fac4d94
Omar <omar_zikry@github.com>
2018-03-03 23:29:05 +0200

    more

---
+++ omarzikry2/README.md
@@ -0,0 +1,1 @@
+# omarzikry.com
---
+++ omarzikry2/js/scripts.js
@@ -0,0 +1,73 @@
+//navbar background
+
+$(document).ready(function() {     
+    $('#link1').hover(function(){     
+        $('#mySidenav').addClass('back');    
+    },     
+    function(){    
+        $('#mySidenav').removeClass('back');     
+    });
+});   
+
+
+$(document).ready(function() {     
+    $('#link2').hover(function(){     
+        $('#mySidenav').addClass('back1');    
+    },     
+    function(){    
+        $('#mySidenav').removeClass('back1');     
+    });
+});   
+
+$(document).ready(function() {     
+    $('#link3').hover(function(){     
+        $('#mySidenav').addClass('back2');    
+    },     
+    function(){    
+        $('#mySidenav').removeClass('back2');     
+    });
+});   
+
+
+$(document).ready(function() {     
+    $('#link4').hover(function(){     
+        $('#mySidenav').addClass('back3');    
+    },     
+    function(){    
+        $('#mySidenav').removeClass('back3');     
+    });
+});
+$(document).ready(function() {     
+    $('#link5').hover(function(){     
+        $('#mySidenav').addClass('back4');    
+    },     
+    function(){    
+        $('#mySidenav').removeClass('back4');     
+    });
+});
+
+$(document).ready(function() {     
+    $('.nav-btn').click(function(){
+        $('nav').toggleClass('showing');
+        $('.nav-btn').toggleClass('close');
+        $('.text').toggleClass('nav-showing');
+    })
+});
+///change navigation icon color on scrolling
+$(window).scroll(function(){
+    if ($(document).scrollTop() > 650){
+        $('div.dash').addClass('black');
+    }
+    else{
+        $('div.dash').removeClass('black');
+    }
+});
+
+
+//============
+// Animation
+//============
+
+AOS.init({
+    duration: 1200,
+  })
