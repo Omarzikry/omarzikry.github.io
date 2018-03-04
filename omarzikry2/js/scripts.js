fe27aa46066505fd58a52b77d74452b4f7cae175
Omar <omar_zikry@github.com>
2018-03-04 14:19:16 +0200

    update

--- omarzikry2/js/scripts.js
+++ omarzikry2/js/scripts.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
     $('.nav-btn').click(function(){
         $('nav').toggleClass('showing');
         $('.nav-btn').toggleClass('close');
-        $('.text').toggleClass('nav-showing');
+        $('.arrow').toggleClass('nav-showing');
     })
 });
 ///change navigation icon color on scrolling
@@ -71,3 +71,82 @@ $(window).scroll(function(){
 AOS.init({
     duration: 1200,
   })
+
+
+
+  //smooth scroll
+
+  // Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
+
+
+
+
+
+/*///tilt panel
+
+var $body = $('body'),
+	$panel = $('.panel'),
+	$pContent = $('.panel__content'),
+	$img = $('.panel__img-col');
+
+function initTilt() {
+	TweenMax.set([$pContent, $img], { transformStyle: "preserve-3d" });
+
+	$body.mousemove(function(e) {
+		var sxPos = e.pageX / $panel.width() * 100 - 100;
+		var syPos = e.pageY / $panel.height() * 100 - 100;
+		TweenMax.to($pContent, 2, {
+			rotationY: 0.03 * sxPos,
+			rotationX: -0.03 * syPos,
+			transformPerspective: 500,
+			transformOrigin: "center center -400",
+			ease: Expo.easeOut
+		});
+		TweenMax.to($img, 2, {
+			rotationY: 0.03 * sxPos,
+			rotationX: -0.03 * syPos,
+			transformPerspective: 500,
+			transformOrigin: "center center -200",
+			ease: Expo.easeOut
+		});
+	});
+};
+
+initTilt();
+
+console.clear();*/
